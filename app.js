5ad9d4aef2ab44362157d2b2ab2f236352a4d08b
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2022-05-12 21:22:59 +0900

    implement painting

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,26 +13,30 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
 
 function onMousemove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x,y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMousemove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
